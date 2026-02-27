f5b9fd2705310cdb6594d5b03cc48fb9731a881f
CalzinoSaporito17 <dimaiokristian18@gmail.com>
2026-02-27 16:21:08 +0100

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
     const { tag } = req.query;
     // Sostituisci con la tua vera API KEY di Supercell
-    const apiKey = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjRiZjQ2ZjQ2LWQxYWUtNDVkYy05ODJkLTM2ZTYwNzk4M2I5ZSIsImlhdCI6MTc3MjE5NTQ5MSwic3ViIjoiZGV2ZWxvcGVyLzUwOWZiZGMxLTI4MTMtMjU1Mi0xMWZhLTFlYzg2MzVlOTU3NSIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyI5My4xNDkuMTMyLjIxNSJdLCJ0eXBlIjoiY2xpZW50In1dfQ.-JptI-_5ysc5i2YNDPOs7Sq2XlhEIv6t-wNhretdJzUXiiC8ilSsXx2sCCAI28s2ciYlqYMDkv8TXaGredK1pA"; 
+    const apiKey = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImYwMDA3YWVhLWMyNDAtNDFmNi05MjE0LTIxOTJkYjJlMjBjYyIsImlhdCI6MTc3MjIwNTYzMywic3ViIjoiZGV2ZWxvcGVyLzUwOWZiZGMxLTI4MTMtMjU1Mi0xMWZhLTFlYzg2MzVlOTU3NSIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyI5OC44MS4xNzguMTk2Il0sInR5cGUiOiJjbGllbnQifV19.6Nmyp1BbTBU4W12_amCfVqvFFqt04QZiA083dsgUGfR5X2DfOgDtaUSNNRWsJfroUWPFWQHviPUy0FZ_gjBV9Q"; 
 
     try {
         const response = await fetch(`https://api.clashroyale.com/v1/clans/%23${tag.replace('#', '')}/currentriverrace`, {
@@ -14,3 +14,4 @@ export default async function handler(req, res) {
     }
 
 }
+
